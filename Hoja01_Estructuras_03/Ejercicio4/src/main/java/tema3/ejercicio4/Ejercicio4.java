f31ceba7dd1a5d0361262133c448f08f33b54bc6
ataIES <119513428+ataIES@users.noreply.github.com>
2023-10-18 16:56:07 +0200

    Add files via upload

---
+++ Hoja01_Estructuras_03/Ejercicio4/src/main/java/tema3/ejercicio4/Ejercicio4.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package tema3.ejercicio4;
+
+/**
+ *
+ * @author daw1
+ */
+public class Ejercicio4 {
+
+    public static void main(String[] args) {
+        Billete billete=new Billete(810,8);
+        System.out.printf("Distancia recorrida: %d \nNúmero de días: %d \nImporte total: %.2f",billete.getDistancia(),billete.getN_dias(),billete.importe());
+    }
+}

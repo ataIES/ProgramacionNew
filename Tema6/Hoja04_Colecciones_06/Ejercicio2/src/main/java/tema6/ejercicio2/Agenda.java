0cf451aa994024bb8c17795701f32365a1e0f5fb
ataIES <adrian.tresgalloarozamena@iesmiguelherrero.com>
2024-04-11 11:37:55 +0200

    Acabar ejercicio 2

--- Tema6/Hoja04_Colecciones_06/Ejercicio2/src/main/java/tema6/ejercicio2/Agenda.java
+++ Tema6/Hoja04_Colecciones_06/Ejercicio2/src/main/java/tema6/ejercicio2/Agenda.java
@@ -4,6 +4,7 @@
  */
 package tema6.ejercicio2;
 
+import java.util.Iterator;
 import java.util.SortedSet;
 import java.util.TreeSet;
 
@@ -12,10 +13,84 @@ import java.util.TreeSet;
  * @author DAW125
  */
 public class Agenda {
-    private SortedSet<Persona>agenda;
-    
-    public Agenda(){
-        this.agenda=new TreeSet<>();
+
+    private SortedSet<Persona> agenda;
+
+    public Agenda() {
+        this.agenda = new TreeSet<>();
     }
-    
+
+    public void aniadirPersona() {
+        try {
+            Persona p = new Persona();
+            if (agenda.add(p)) {
+                System.out.println("Persona " + p.getNombre() + " " + p.getApellidos() + " añadido correctamente");
+            } else {
+                throw new Exception("Error, no se pudo añadir a la persona " + p.getNombre() + " " + p.getApellidos());
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public void borrarPersona(String dni) {
+        try {
+            Persona personaBorrada = buscarPersona(dni);
+            if (personaBorrada != null) {
+                if (agenda.remove(personaBorrada)) {
+                    System.out.println("Persona " + dni + " borrada correctamente");
+                } else {
+                    throw new Exception("Error, no se pudo borrar a la persona " + dni);
+                }
+            } else {
+                throw new NullPointerException("Error, no existe la persona con dni " + dni);
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public String mostrarAgenda() {
+        String cadena = "\n\t---Agenda---";
+        for (Persona p : agenda) {
+            cadena += p.mostrarPersona();
+        }
+
+        return cadena;
+    }
+
+    public Persona buscarPersona(String dni) {
+        Persona personaEncontrada = null;
+        Iterator<Persona> it = agenda.iterator();
+        while (it.hasNext()) {
+            Persona p = it.next();
+            if (p.getDni().equalsIgnoreCase(dni)) {
+                personaEncontrada = p;
+            }
+        }
+        return personaEncontrada;
+    }
+
+    public void modificarDni(String dni) {
+        try {
+            Persona personaEncontrada = buscarPersona(dni);
+            String dniNuevo = "";
+
+            if (personaEncontrada != null) {
+                dniNuevo = Teclado.introDni("Introduce el nuevo dni: ");
+                personaEncontrada.setDni(dniNuevo);
+                if (personaEncontrada.getDni().equalsIgnoreCase(dniNuevo)) {
+                    System.out.println("Dni modificado correctamente");
+                } else {
+                    throw new Exception("Error, no se pudo modificar su dni");
+                }
+            } else {
+                throw new NullPointerException("Error, la persona con dni " + dni + " no existe");
+            }
+        } catch (NullPointerException n) {
+            System.out.println(n.getMessage());
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
 }
--- Tema6/Hoja04_Colecciones_06/Ejercicio2/src/main/java/tema6/ejercicio2/Ejercicio2.java
+++ Tema6/Hoja04_Colecciones_06/Ejercicio2/src/main/java/tema6/ejercicio2/Ejercicio2.java
@@ -4,6 +4,8 @@
 
 package tema6.ejercicio2;
 
+import java.util.Scanner;
+
 /**
  *
  * @author DAW125
@@ -11,8 +13,43 @@ package tema6.ejercicio2;
 public class Ejercicio2 {
 
     public static void main(String[] args) {
-        String nombre=Teclado.introNombre("Introduce su nombre: ");
-        String apellidos=Teclado.introApellidos("Introduce sus dos apellidos: ");
-        System.out.println(nombre+" "+apellidos);
+        int opc=0;
+        Agenda agenda=new Agenda();
+        do{
+            menu();
+            opc=new Scanner(System.in).nextInt();
+            switch(opc){
+                case 1->agenda.aniadirPersona();
+                
+                case 2->{
+                    String dniABorrar=Teclado.introDni("Introduce el dni de la persona a borrar: ");
+                    agenda.borrarPersona(dniABorrar);
+                }
+                case 3->System.out.println(agenda.mostrarAgenda());
+                
+                case 4->{
+                    String dniABuscar=Teclado.introDni("Introduce el dni de la persona a buscar: ");
+                    Persona personaEncontrada=agenda.buscarPersona(dniABuscar);
+                    System.out.println(personaEncontrada.mostrarPersona());
+                }
+                case 5->{
+                    String dniAModificar=Teclado.introDni("Introduce el dni de la persona a modificar: ");
+                    agenda.modificarDni(dniAModificar);
+                }
+                case 6->System.out.println("Muchas Gracias!!");
+                
+                default->System.out.println("Error, opción incorrecta");
+            }
+        }while(opc!=6);
+    }
+    public static void menu(){
+        System.out.printf("\n\t---Menú de Agenda---"
+                + "\n1.Insertar persona a la agenda"
+                + "\n2.Borrar una persona"
+                + "\n3.Mostrar agenda"
+                + "\n4.Buscar una persona por dni"
+                + "\n5.Modificar dni"
+                + "\n6.Salir"
+                + "\nIntroduce una opción: ");
     }
 }
